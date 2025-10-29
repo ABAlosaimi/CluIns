7160bc738b5dd67bed186ada2462eace9b5319c6
ABAlosaimi <afalih.alosaimi@gmail.com>
2025-10-29 22:11:48 +0300

    added reporting func

--- main/main.go
+++ main/main.go
@@ -5,6 +5,10 @@ import (
 	"log"
 	"net"
 	"net/http"
+	"time"
+
+	"github.com/shirou/gopsutil/cpu"
+	"github.com/shirou/gopsutil/v4/mem"
 )
 
 func getLocalIP() (string, error) {
@@ -47,10 +51,58 @@ func main() {
 	if err := http.Serve(listener, nil); err != nil {
 		log.Fatalf("the service unable to start: %v", err)
 	}
-	
+
 }
 
 func reportCpuAndMemData(res http.ResponseWriter, req *http.Request) {
 
+	res.Header().Set("Content-Type", "text/event-stream")
+	res.Header().Set("Cache-Control", "no-cache")
+	res.Header().Set("Connection", "keep-alive")
+
+	memTime := time.NewTicker(time.Second)
+	defer memTime.Stop()
+
+	cpuTime := time.NewTicker(time.Second)
+	defer cpuTime.Stop()
+
+	clientGone := req.Context().Done()
+	rc := http.NewResponseController(res)
+
+	for {
+		select{
+		case <- clientGone:
+			fmt.Println("the client is disconnected")
+			return
+		case <- memTime.C:
+			m, err := mem.VirtualMemory()
+			if err != nil {
+				log.Fatalf("unable to get memory data: %v", err.Error())
+				return
+			}
+
+			if _, err := fmt.Fprintf(res, "event:memory\n data: Total: %d, Used: %d, Pers: %.2f,\n\n", m.Total, m.Used, m.UsedPercent); err != nil {
+					log.Fatalf("unable to write back to the client: %v", err.Error())
+					return
+			}
+
+			rc.Flush()
+
+		case <- cpuTime.C:
+			cpu, err := cpu.Times(false) 
+			if err != nil {
+				log.Fatalf("unable to get memory data: %v", err.Error())
+				return
+			}
+
+			if _, err := fmt.Fprintf(res, "event:cpu\n data: User: %.2f, System: %.2f, Idle : %.2f,\n\n", cpu[0].User, cpu[0].System, cpu[0].Idle); err != nil {
+					log.Fatalf("unable to write back to the client: %v", err.Error())
+					return
+			}
+
+			rc.Flush()
+		}
+
+	}
 
 }
